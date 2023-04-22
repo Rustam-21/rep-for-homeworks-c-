cdc95ff2d290b5a1edcff9779f30af2e7b6b1526
Kalybaev Rustam <me@kalybaev.ru>
2023-04-22 14:30:17 +0500

    homework task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+System.Console.WriteLine("Введите число");
+string text = Console.ReadLine();
+int number = int.Parse(text);
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine($"Да, {number} - четное число");
+}
+else
+{
+    System.Console.WriteLine($"Нет, {number} - нечетное число");
+}
